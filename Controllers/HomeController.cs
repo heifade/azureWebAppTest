09c70abecaabe9f8fb21804c0c757e7e3a3829c1
bis\dongjd <heifade@126.com>
2021-04-14 21:06:22 +0800

    no message

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -21,7 +21,7 @@ namespace apiManageTest.Controllers
         [HttpPost]   
         public JsonResult postSum(FormData formData)
         {
-            var sum = (int.Parse(formData.value1) + int.Parse(formData.value2)) * 10;
+            var sum = (int.Parse(formData.value1) + int.Parse(formData.value2)) * 20;
             return Json(sum);
         }
 
